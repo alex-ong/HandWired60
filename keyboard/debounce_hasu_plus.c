993603f9636a3b4f35743db90473ddaca24c85b2
alex-ong <the.onga@gmail.com>
2018-01-20 03:43:37 +1100

    Squished hasuplus into an array of uint8_t's instead of a bool and two uint8s

--- keyboard/debounce_hasu_plus.c
+++ keyboard/debounce_hasu_plus.c
@@ -16,18 +16,30 @@ Per key debouncing rather than whole-matrix debouncing.
     #define DEBOUNCE_RELEASE 5
 #endif
 
-#define MAX_DEBOUNCE 5
-
 #define CHECK_BIT(var,pos) ((var) & (1 <<(pos)))
 #define CLEAR_BIT(var,pos) ((var) & ~(1 << (pos)))
 #define SET_BIT(var,pos) ((var) | (1 << (pos)))
 
-static bool is_debouncing[MATRIX_ROWS*MATRIX_COLS];
-static uint8_t checks_remaining[MATRIX_ROWS*MATRIX_COLS];
-static uint8_t last_check[MATRIX_ROWS*MATRIX_COLS];
 static matrix_row_t matrix_debouncing[MATRIX_ROWS];
 
+
 static uint8_t current_time;
+
+//bits 1,2,3 - bounces remaining
+//bits 6,7,8 - last timestamp with debounce, modulo MAX_DEBOUNCE
+typedef uint8_t debounce_t;
+#define IS_DEBOUNCING(var) (var)
+#define CHECKS_REMAINING(var) (((var) >> 5) & 0b111)
+#define MAX_DEBOUNCE 0b111
+#define IS_TIME(var, time) (((var) & (MAX_DEBOUNCE)) == time)
+static debounce_t debounce_data[MATRIX_ROWS*MATRIX_COLS];
+
+
+
+static inline debounce_t set_debounce(uint8_t checks)
+{         
+    return ((checks << 5) | (current_time % MAX_DEBOUNCE));               
+}
 
 void debounce_matrix_init(void)
 {	
@@ -38,53 +50,47 @@ void debounce_matrix_init(void)
     
     for (uint8_t i = 0; i < MATRIX_ROWS*MATRIX_COLS; i++)
     {
-        is_debouncing[i] = false;
-        checks_remaining[i] = 0;
-        last_check[i] = 0;
+        debounce_data[i] = 0;
     }
 }
 
 void update_debounce_matrix(matrix_row_t* raw_values, matrix_row_t* output_matrix)
 {
-    current_time = timer_read(); //update timer.
+    current_time = timer_read() % MAX_DEBOUNCE; //update timer.
+    debounce_t* data = debounce_data;
     for (uint8_t row_num = 0; row_num < MATRIX_ROWS; row_num++) {
         matrix_row_t cols = raw_values[row_num];
         matrix_row_t result = output_matrix[row_num];
         matrix_row_t existing = matrix_debouncing[row_num];
+        
         for (uint8_t col_num = 0; col_num < MATRIX_COLS; col_num++) {
             bool new_col = CHECK_BIT(cols, col_num);
             bool old_col = CHECK_BIT(existing, col_num);
-            uint8_t index = row_num * MATRIX_COLS + col_num;
-            if (is_debouncing[index]) {                
-                if (current_time != last_check[index]) { //otherwise skip check.
+            
+            if (IS_DEBOUNCING(*data)) {                
+                if (!IS_TIME(*data, current_time)) { //otherwise skip check.
                     if (new_col == old_col) {
-                        checks_remaining[index] = checks_remaining[index] - 1;
-                        if (checks_remaining[index] == 0) 
+                        uint8_t remaining_checks = CHECKS_REMAINING(*data);
+                        if (remaining_checks == 1) 
                         {
                             if (new_col) {
                                 result = SET_BIT(result, col_num);
                             } else {
                                 result = CLEAR_BIT(result, col_num);
                             }
-                            is_debouncing[index] = false;
+                            *data = 0;
                         } else {
-                            last_check[index] = current_time;
+                            *data = set_debounce(remaining_checks - 1);
                         }                        
                     } else { //reset checks because value changed.
-                        if (new_col) {                        
-                            last_check[index] = current_time;
-                            checks_remaining[index] = DEBOUNCE_PRESS;
-                        } else {
-                            last_check[index] = current_time;
-                            checks_remaining[index] = DEBOUNCE_RELEASE;
-                        }
+                        *data = set_debounce(new_col ? DEBOUNCE_PRESS : DEBOUNCE_RELEASE);
                     }
                 }
             } else if (new_col != old_col) { //not debouncing. time to add a debouncer
-                is_debouncing[index] = true;
-                last_check[index] = current_time;
-                checks_remaining[index] = new_col ? DEBOUNCE_PRESS : DEBOUNCE_RELEASE;                
-            }            
+                *data = set_debounce(new_col ? DEBOUNCE_PRESS : DEBOUNCE_RELEASE);
+            }      
+            
+            data++;
         }
         
         matrix_debouncing[row_num] = cols;        
