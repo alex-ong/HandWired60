e06a39710727ca673c292b7b7c826225509c747f
alex-ong <alex.ong@unsw.edu.au>
2018-01-23 14:14:28 +1100

    Added debounce_hasu_noblock.c

---
+++ keyboard/debounce_hasu_noblock.c
@@ -0,0 +1,55 @@
+/*
+Original TMK algorithm
+Has debounce timers per row.
+*/
+#include "matrix.h"
+#include "debounce_matrix.h"
+#include <util/delay.h>
+#include "timer.h"
+#ifndef DEBOUNCE
+    #define DEBOUNCE 5
+#endif
+
+static uint8_t debouncing = DEBOUNCE; //debouncing timer in milliseconds.
+static matrix_row_t matrix_debouncing[MATRIX_ROWS];
+
+static uint8_t last_tick = 0;
+
+void debounce_matrix_init(void)
+{	
+    for (int i = 0; i < MATRIX_ROWS; i++)
+    {
+        matrix_debouncing[i] = 0;
+    }
+}
+
+bool tick_changed(void)
+{
+    uint8_t new_tick = timer_read() % 0xFF; //update timer.
+    bool result = new_tick == last_tick;
+    last_tick = new_tick;
+    return result;
+}
+
+void update_debounce_matrix(matrix_row_t* raw_values, matrix_row_t* output_matrix)
+{
+    bool new_tick = tick_changed();
+    for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
+        matrix_row_t cols = raw_values[i];
+        if (matrix_debouncing[i] != cols) {
+            matrix_debouncing[i] = cols;
+            debouncing = DEBOUNCE;
+        }
+
+        if (debouncing) {
+            if (new_tick) {
+                if (--debouncing) {
+                    for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
+                        output_matrix[i] = matrix_debouncing[i];
+                    }
+                }
+            }
+        }
+    }
+    
+}
